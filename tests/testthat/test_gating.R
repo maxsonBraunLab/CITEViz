668b15b4c3e65b816d998291214d409b77d10009
gartician <kongga2017@gmail.com>
2023-01-19 15:42:52 -0800

    add a simple test for SetSubsetName

--- tests/testthat/test_gating.R
+++ tests/testthat/test_gating.R
@@ -28,7 +28,7 @@ test_that("create_gating_df creates properly formatted empty dataframe", {
 #---------------testing Gate()-----------------#
 
 # create Gate objects for testing
-test_gate1 <- Gate(
+test_gate_1 <- Gate(
     counter = as.integer(1),
     assay_name = "test1",
     input_cells = list(c("a", "b", "c")),
@@ -48,18 +48,18 @@ test_gate1 <- Gate(
 
 
 test_that("Gate() create correctly formatted Gate object", {
-    expect_type(test_gate1@assay_name, "character")
-    expect_type(test_gate1@num_input_cells, "integer")
-    expect_type(test_gate1@input_cells, "list")
-    expect_equal(test_gate1@x_axis, "ADT-A")
-    expect_equal(test_gate1@y_axis, "ADT-B")
+    expect_type(test_gate_1@assay_name, "character")
+    expect_type(test_gate_1@num_input_cells, "integer")
+    expect_type(test_gate_1@input_cells, "list")
+    expect_equal(test_gate_1@x_axis, "ADT-A")
+    expect_equal(test_gate_1@y_axis, "ADT-B")
 })
 
 #---------------testing update_gating_df()-----------------#
 
 # update_gating_df() requires a list of gate objects to choose from
 
-test_gate2 <- Gate(
+test_gate_2 <- Gate(
     counter = as.integer(2),
     assay_name = "test2",
     input_cells = list(c("a", "b")),
@@ -77,14 +77,22 @@ test_gate2 <- Gate(
     pct_subset_from_total = 10
 )
 
-test_gate_list <- list("gate1" = test_gate1, "gate2" = test_gate2)
+test_gate_list <- list("gate1" = test_gate_1, "gate2" = test_gate_2)
 
 test_df <- update_gating_df(gate_name_string = "gate1", reactive_gate_list = test_gate_list, temp_gating_df = test_df)
 test_df <- update_gating_df(gate_name_string = "gate2", reactive_gate_list = test_gate_list, temp_gating_df = test_df)
 
 test_that("update_gating_df() updated gating dataframes correctly", {
     expect_equal(test_df$Assay_Name, c("test1", "test2"))
-    expect_equal(test_df$Num_Input_Cells[2], test_gate2@num_input_cells)
-    expect_equal(test_df$Num_Input_Cells[1], test_gate1@num_input_cells)
-    expect_equal(test_df$Input_X_Coordinates[[1]], test_gate1@input_coords$x)
+    expect_equal(test_df$Num_Input_Cells[2], test_gate_2@num_input_cells)
+    expect_equal(test_df$Num_Input_Cells[1], test_gate_1@num_input_cells)
+    expect_equal(test_df$Input_X_Coordinates[[1]], test_gate_1@input_coords$x)
+})
+
+#---------------testing SetSubsetName()-----------------#
+
+test_gate_3 <- SetSubsetName(test_gate_1, "test_cells_C")
+
+test_that("Changing a gate's label", {
+    expect_equal(test_gate_3@name_subset_cells, "test_cells_C")
 })
